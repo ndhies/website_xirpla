d14c8b4e66d1f7ef4fd3697e7e92793ee6d97b04
Levi Ackerman's Gf <ndhies17@gmail.com>
2024-11-13 07:22:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>XI PPLG A</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <style>
+    .gradient-text {
+      background: linear-gradient(360deg, #7FC8F8, #5AA9E6);
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+  </style>
+</head>
+
+<body>
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand mx-5 fw-bolder" href="#">XI RPL A <img class="rounded-circle mx-2 mb-1"
+          src="Assets/Logo RPL.jpg" alt="" width="20px"></a>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button class="btn btn-primary fw-bolder" type="button">HUBUNGI</button>
+        <button class="btn bg-primary bg-gradient mx-4 fw-bolder text-light" type="button">MASUK</button>
+      </div>
+    </div>
+  </nav>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col mt-5">
+        <h1 class="text-start mt-5 fw-bold">Selamat Datang</h1>
+        <h1 class="text-start fw-bold">Di <span class="gradient-text">XI RPL A!</span></h1>
+        <p class="text-start">Kami, kelas XI RPL A, berfokus pada inovasi di Rekayasa Perangkat Lunak. Website ini
+          memperkenalkan kegiatan, prestasi, dan layanan kami. Semoga bermanfaat bagi semua.</p>
+        <button type="button" class="btn bg-primary bg-gradient btn-lg fw-bold text-light" disabled>BELANJA
+          SEKARANG</button>
+      </div>
+      <div class="col text-center">
+        <img src="Assets/programmer.png" width="600px" alt="...">
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bg-dark">
+    <div class="row">
+      <div class="col-5 text-light">
+        <h1 class="fw-bold mt-5 mb-5 text-start mx-5 p-5">APA SIH XI RPL A ITU?</h1>
+      </div>
+      <div class="col text-justify fw-bold text-light mt-5 mb-4">
+        <div class="">
+          <p class=" mt-5">XI RPL A adalah kelas tingkat XI di jurusan Rekayasa Perangkat Lunak (RPL) yang berfokus pada
+            pengembangan keterampilan dalam merancang, membangun, dan mengelola perangkat lunak. Kelas ini terdiri dari siswa-siswa
+            yang antusias dalam mempelajari teknologi, pemrograman, dan inovasi di dunia digital, dengan tujuan untuk
+            menjadi ahli di bidang teknologi informasi dan komunikasi.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-5">
+        <img src="Assets/shopping.png" width="600px" alt="...">
+      </div>
+      <div class="col">
+        <h2 class="gradient-text text-start mt-5  fw-bold mx-n4">Produk Berkualitas, Harga Mantap dan Proses Cepat</h2>
+        <p class="text-start mt-4">Kami menyediakan berbagai macam produk berkualitas dengan harga terjangkau. Kenapa
+          kamu harus membeli produk kami?</p>
+        <ul>
+          <li>Menarik</li>
+          <li>Berkualitas</li>
+          <li>Proses Cepat</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div class="container bg-dark rounded-4 px-5 mb-5">
+      <h2 class="gradient-text fw-bold mt-5 text-center mx-5 pt-5 text-uppercase">Metode Pembayaran</h2>
+      <h6 class="text-light text-center">Kami menyediakan berbagai metode pembayaran yang bisa kamu pilih sesuai
+        keinginanmu</h6>
+      <div class="container text-center mx-2">
+        <div class="row mt-5 pb-5 mx-5">
+          <div class="col text-light pb-5 pt-2">
+            <div class="bg-body w-100 rounded-4">
+              <div class="pt-4 pb-4">
+                <img src="Assets/image 3.png" width="250px" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="col text-light pb-5 pt-2">
+            <div class="bg-body w-100 rounded-4 pt-3 pb-3">
+              <img src="Assets/image 2.png" width="250px" alt="">
+            </div>
+          </div>
+          <div class="col text-light pb-5 pt-2">
+            <div class="bg-body w-100 rounded-4 pt-3 pb-3 me-4">
+              <img src="Assets/image 1.png" width="258px" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bg-dark mt-5 my-5 px-5 pt-4">
+    <div class="mb-3 text-light d-flex justify-content-between">
+      <div class="d-flex flex-row mb-3">
+        <img class="rounded-circle" src="Assets/Logo RPL.jpg" width="45px" alt="">
+        <h4 class="p-2 fw-bold">XI RPL A</h4>
+      </div>
+      <div class="d-flex flex-row mb-3">
+        <div class="p-2">PROFILE</div>
+        <div class="p-2">SHOP</div>
+        <div class="p-2">CONTACT</div>
+      </div>
+    </div>
+  </div>     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+      integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+      crossorigin="anonymous"></script>
+    </script>
+</body>
+
+</html>
